c9d7faa808bfe392b5c1fb49b6b1d29406b5ea22
Kemal <lepuzanovickemal@gmail.com>
2023-10-28 00:35:58 +0200

    public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/all-comments"],
   ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/scrape"],
 });
 
