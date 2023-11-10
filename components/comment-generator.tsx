2cf913c24feb623c5bd03d378582ca646ca2a491
Kemal Lepuzanović <95963030+Gusnhde2@users.noreply.github.com>
2023-11-10 20:59:16 +0100

    Update comment-generator

--- components/comment-generator.tsx
+++ components/comment-generator.tsx
@@ -34,7 +34,7 @@ export default function CommentGenerator({
       });
       if (response.ok) {
         const data = await response.json();
-        setComment(data.message.content);
+        setComment(data?.message?.content);
         setLoading(false);
       }
     } catch (error) {
